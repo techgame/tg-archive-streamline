27676495d37b4aafdc9d20e747999267e1689c27
"Shane Holloway ext:(%22) <shane.holloway@ieee.org>
2007-08-10 16:44:46 -0600

    Added some documentation to streamline setup script

--- setupScripts.py
+++ setupScripts.py
@@ -19,4 +19,13 @@ from distutils.core import setup
 setup(
     name='TG.streamline scripts', version='0.1',
     scripts=['scripts/ipysl.py', 'scripts/pysl.py', 'scripts/appsl.py'])
+
+if os.name.startswith('win'):
+    print
+    print "NOTE:"
+    print "For these scripts to run correctly, you need to make sure a few environment variables are set."
+    print
+    print "  PATH needs to include: \"%s\\Scripts\"" % (sys.exec_prefix,)
+    print "  PATHEXT needs to include: \".PY\""
+    print
         
