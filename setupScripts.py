2f1188fa3b2e154cc9fc4bbb6d1c99fc94d624c2
"Shane Holloway ext:(%22) <shane.holloway@ieee.org>
2007-08-10 16:50:08 -0600

    Moved documentation to before setup() -- it exits without returning

--- setupScripts.py
+++ setupScripts.py
@@ -17,16 +17,15 @@ from distutils.core import setup
 #~ Definitions 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+print
+print "NOTE: For these scripts to run correctly, you need to make sure a few environment variables are set."
+print
+print "  PATH needs to include: \"%s\\Scripts\"" % (sys.exec_prefix,)
+print "  PATHEXT needs to include: \".PY\""
+print
+print
+        
 setup(
     name='TG.streamline scripts', version='0.1',
     scripts=['scripts/ipysl.py', 'scripts/pysl.py', 'scripts/appsl.py'])
 
-if os.name.startswith('win'):
-    print
-    print "NOTE:"
-    print "For these scripts to run correctly, you need to make sure a few environment variables are set."
-    print
-    print "  PATH needs to include: \"%s\\Scripts\"" % (sys.exec_prefix,)
-    print "  PATHEXT needs to include: \".PY\""
-    print
-        
